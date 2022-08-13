c597a9a8f16a28dc53021cc34158e91e2f986c28
Venancio Orozco <4390221+v3nant@users.noreply.github.com>
2022-08-12 21:56:07 -0600

    Initial Project

---
+++ HelloGit/Controllers/VehicleController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloGit.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VehicleController : ControllerBase
+    {
+
+        [HttpGet("list")]
+        public IEnumerable<Vehicle> List()
+        {
+            List<Vehicle> vehicleList = new List<Vehicle>();
+            vehicleList.Add(
+                new Vehicle
+                {
+                    Maker = "Subaru",
+                    Model = "Impressa",
+                    Price = 15000
+                });
+            vehicleList.Add(
+           new Vehicle
+           {
+               Maker = "Mazda",
+               Model = "3",
+               Price = 12000
+           });
+            return vehicleList;
+        }
+
+        [HttpGet()]
+        public Vehicle Get()
+        {
+            return new Vehicle
+            {
+                Maker = "Subaru",
+                Model = "Impressa",
+                Price = 15000
+            };
+        }
+    }
+}
---
+++ HelloGit/Vehicle.cs
@@ -0,0 +1,9 @@
+﻿namespace HelloGit
+{
+    public class Vehicle
+    {
+        public string Maker { get; set; }
+        public string Model { get; set; }
+        public double Price { get; set; }
+    }
+}
